066d4ca989c68f3f550ba962267787e74d089ecf
Jordi <info@alquilerjoven.com>
2025-03-20 15:55:29 +0100

    chore: response type

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -577,12 +577,15 @@ def load_oauth_providers():
                 client_id=OAUTH_CLIENT_ID.value,
                 client_secret=OAUTH_CLIENT_SECRET.value,
                 server_metadata_url=OPENID_PROVIDER_URL.value,
+                audience= OAUTH_AUDIENCE.value,
+                response_type= OAUTH_RESPONSE_TYPE.value,
                 client_kwargs={
                     "scope": OAUTH_SCOPES.value,
-                    "audience": OAUTH_AUDIENCE.value,
-                    "response_type": OAUTH_RESPONSE_TYPE.value
+                    #"audience": OAUTH_AUDIENCE.value,
+                    #"response_type": OAUTH_RESPONSE_TYPE.value
 
                 },
+                
                 redirect_uri=OPENID_REDIRECT_URI.value,
             )
 
